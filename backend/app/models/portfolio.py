44773ead7500c5dd6297aacd1937218ccc93b7ab
Finnley Wyllie <u24754120@tuks.co.za>
2026-05-09 17:36:33 +0200

    added portfolio model

---
+++ backend/app/models/portfolio.py
@@ -0,0 +1,8 @@
+from typing import Optional
+from sqlmodel import Field, SQLModel 
+
+
+class Portfolio(SQLModel,table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(default=None,foreign_key="user.user_id")
+    cash_balance: float = Field(default=0)
