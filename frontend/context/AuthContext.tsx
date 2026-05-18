076a6696457bfeca4a018d17e5dcca23257cf88c
u24641342 <u24641342@tuks.co.za>
2026-05-18 21:31:33 +0200

    Implemented AuthContext and AuthProvider for user authentication management

---
+++ frontend/context/AuthContext.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import {
+    useState,
+    useEffect,
+    useMemo,
+    createContext,
+    useContext,
+    type ReactNode
+} from 'react';
+
+interface User {
+    id: number;
+    email: string;
+    password_hash: string;
+    full_name: string;
+}
+
+interface AuthContextType {
+    user: User | null;
+    token: string | null;
+    login: (token: string, user: User) => void;
+    logout: () => void;
+    isLoading: boolean;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children }: { children: React.ReactNode })
+{
+    const [user, setUser] = useState<User | null>(null);
+    const [token, setToken] = useState<string | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const storedToken = sessionStorage.getItem('token');
+        const storedUser = sessionStorage.getItem('user');
+        if (storedToken && storedUser)
+        {
+            setToken(storedToken);
+            setUser(JSON.parse(storedUser));
+            console.log("Token found:", storedToken);
+        }
+        else
+        {
+            console.log("No token in session storage.");
+        }
+        setIsLoading(false); 
+    }, []);
+    
+    const login = (newToken: string, newUser: User) => {
+        setToken(newToken);
+        setUser(newUser);
+        sessionStorage.setItem('token', newToken);
+        sessionStorage.setItem('user', JSON.stringify(newUser));
+    }
+    
+    const logout = () => {
+        setToken(null);
+        setUser(null);
+        sessionStorage.removeItem('token');
+        sessionStorage.removeItem('user');
+    }
+
+    const memoizedValue = useMemo(() => ({
+        user,
+        token,
+        login,
+        logout,
+        isLoading
+    }), [user, token, isLoading]);
+
+    return (<AuthContext.Provider value={memoizedValue}>{children}</AuthContext.Provider>);
+}
+export function useAuth() {
+    const context = useContext(AuthContext);
+    if (!context) throw new Error("useAuth must be used within AuthProvider");
+    return (context);
+}
