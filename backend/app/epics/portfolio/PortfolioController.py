4c7aab442ca07e20e62354c769620eb5225cd3e1
Finnley Wyllie <u24754120@tuks.co.za>
2026-05-18 13:09:32 +0200

    added route to controller

--- backend/app/epics/portfolio/PortfolioController.py
+++ backend/app/epics/portfolio/PortfolioController.py
@@ -3,7 +3,7 @@ from sqlmodel import Session
 from ...core.security import get_current_user
 from ...models import User
 
-from .PortfolioDTOs import EpicStatusDTO, ExecuteTradeDTO,ExecuteTradeResponseDTO
+from .PortfolioDTOs import EpicStatusDTO, ExecuteTradeDTO,ExecuteTradeResponseDTO, TradeHistoryResponse
 from .PortfolioService import PortfolioService
 from ...database import get_session
 
@@ -18,7 +18,12 @@ router = APIRouter(prefix="/portfolio", tags=["Portfolio"])
 def get_epic_status() -> EpicStatusDTO:
     return PortfolioService.get_status()
 
+@router.get("/accounts/{account_id}",response_model=TradeHistoryResponse)
+def get_trade_history(account_id:int,service:PortfolioService=Depends(get_portfolio_service),current_user:User=Depends(get_current_user))->TradeHistoryResponse:
 
-@router.post("/accounts/{acount_id}", response_model=ExecuteTradeResponseDTO)
-def execute_trade(data:ExecuteTradeDTO,service:PortfolioService=Depends(get_portfolio_service),current_user:User=Depends(get_current_user)) -> ExecuteTradeResponseDTO:
-    return PortfolioService.execute_trade(service,data,current_user)
+    return service.get_transaction_history(account_id,current_user)
+
+
+@router.post("/accounts/{account_id}", response_model=ExecuteTradeResponseDTO)
+def execute_trade(data:ExecuteTradeDTO,account_id:int,service:PortfolioService=Depends(get_portfolio_service),current_user:User=Depends(get_current_user)) -> ExecuteTradeResponseDTO:
+    return service.execute_trade(data,account_id,current_user)
