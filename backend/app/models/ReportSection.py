04cff86e1dcee7f36cc456e457c6ac44874f7cd6
u24603199 <u24603199@tuks.co.za>
2026-05-19 19:27:38 +0200

    Added ReportSection.py

---
+++ backend/app/models/ReportSection.py
@@ -0,0 +1,15 @@
+from decimal import Decimal
+from enum import Enum
+from typing import Optional
+from sqlmodel import Field, SQLModel
+
+class ReportSection(SQLModel, table = True):
+    id:Optional[int] = Field(default=None,primary_key=True)
+    report_id: int = Field(foreign_key="report.id", index=True)
+    ticker: str = Field(default=None)
+    open_price: Decimal = Field(default=None, max_digits=18, decimal_places=4)
+    close_price: Decimal = Field(default=None, max_digits=18, decimal_places=4)
+    pct_change: float = Field(default=None)
+    period_high: Decimal = Field(default=None, max_digits=18, decimal_places=4)
+    period_low: Decimal = Field(default=None, max_digits=18, decimal_places=4)
+    #decimal(18, 4) The king of showing currency in a db schema is back
--- backend/app/models/report.py
+++ backend/app/models/report.py
@@ -1,5 +1,4 @@
 from datetime import datetime,timezone
-from decimal import Decimal
 from enum import Enum
 from typing import Optional
 from sqlmodel import Field, SQLModel
