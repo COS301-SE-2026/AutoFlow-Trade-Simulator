8e121ef60719c6a0e487fedad0d8f695f0408ba6
Finnley Wyllie <u24754120@tuks.co.za>
2026-05-22 01:53:17 +0200

    linting

--- frontend/components/ui/intAccSwitcher.tsx
+++ frontend/components/ui/intAccSwitcher.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/select";
 import {useAccount} from "@/lib/hooks/accountContext";
 import type {InternationalAccount} from "@/lib/types/accounts";
+import Image from "next/image";
 
 interface AccountSelectorProps {
     placeholder?: string;
@@ -57,10 +58,12 @@ export function AccountSelector({
                     <SelectGroup>
                         {accounts?.map((account) => (
                             <SelectItem key={account.id} value={account.id.toString()} className="py-3 text-base">
-                                <img
+                                <Image
                                     src={`https://flagcdn.com/w20/${account.currency_code.substring(0, 2).toLowerCase()}.png`}
                                     className="flag inline-block mr-2 w-5 h-auto"
                                     alt=""
+                                    width={20}
+                                    height={15}
                                 />
                                 {account.currency_code} {account.balance}
                             </SelectItem>
