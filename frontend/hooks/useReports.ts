64c30ace7ce68b0a570ac32d5fdd1208075b9a5c
u24614582 <u24614582@tuks.co.za>
2026-05-21 13:50:22 +0200

    made hook for reports get and post endpoint for frontend component

---
+++ frontend/hooks/useReports.ts
@@ -0,0 +1,54 @@
+'use client';
+import { apiClient } from "@/lib/api";
+import { useCallback, useEffect, useState } from "react";
+
+export interface Reports {
+    id: number,
+    report_id: number,
+    ticker: string,
+    open_price: string,
+    close_price: string,
+    pct_change: number,
+    period_high: string,
+    period_low: string
+}
+
+export function useReports(account_id: number | null) {
+    const [reports, setReports] = useState<Reports[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+    const fetchReports = useCallback(async () => {
+        if (!account_id) return;
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await apiClient(`/reports/`);
+            setReports(response);
+        } catch (error: any) {
+            setError(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }, [account_id]);
+
+    const createReport = async (period: string) => {
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await apiClient(`/reports/`, {
+                method: "POST",
+                body: { period }
+            });
+            setReports(prev => [...prev, response]);
+        } catch (error: any) {
+            setError(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => { fetchReports(); }, [fetchReports]);
+
+    return { reports, loading, error, refetch: fetchReports, createReport }
+}
