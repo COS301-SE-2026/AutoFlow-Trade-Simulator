d9a20fc76a7756ebea6fe7b315cb43564ede5bb1
u24603199 <u24603199@tuks.co.za>
2026-06-25 19:41:14 +0200

    Starting with the implementaion of the sliding TTL

--- backend/app/models/cache/TTL.py
+++ backend/app/models/cache/TTL.py
@@ -1,5 +1,29 @@
 from datetime import datetime, timedelta
 from decimal import Decimal
 from sqlmodel import Session
+from .CacheMarketDataDTO import CacheMarketDataDTO
 
 class TTL:
+    market_data: CacheMarketDataDTO
+
+    def __init__(self, cache_dto: CacheMarketDataDTO)
+        self.market_data = cache_dto
+
+    def add_to_cache(self, db:Session)
+
+        now = datetime.now()
+        ttl_window = timedelta(minutes=30)
+        expiry = now + ttl_window
+
+        asset_id = self.market_data.asset_id
+
+        #Need a try catch for DB stuff just like asp.net
+        try:
+            statement = select(MarketDataCache).where(MarketDataCache.asset_id == asset_id)
+            record = db.exec(statement).first()
+
+            #The record exsists
+            if record:
+                
+
+        if self.market_data.asset_id is None:
