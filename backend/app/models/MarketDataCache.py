9535401c9759aca4201945bc53436bdfebd7248f
u24603199 <u24603199@tuks.co.za>
2026-07-01 13:47:42 +0200

    Split up market data cache into 2 tables as per request

--- backend/app/models/MarketDataCache.py
+++ backend/app/models/MarketDataCache.py
@@ -1,5 +1,3 @@
-from datetime import datetime, timedelta
-from typing import Optional
 from decimal import Decimal
 from sqlmodel import Field, SQLModel
 
@@ -13,10 +11,4 @@ class MarketDataCache(SQLModel, table=True):
     ask_price: Decimal = Field(max_digits=18, decimal_places=4, nullable=False)
     bid_size: int = Field(nullable=False)
     ask_size: int = Field(nullable=False)
-    total_volume: int = Field(nullable=False)
-    open_price: Decimal = Field(max_digits=18, decimal_places=4, nullable=False)
-    high_price: Decimal = Field(max_digits=18, decimal_places=4, nullable=False)
-    low_price: Decimal = Field(max_digits=18, decimal_places=4, nullable=False)
-    official_close: Optional[Decimal] = Field(default=None, max_digits=18, decimal_places=4)
     trading_status: str = Field(default="Trading", nullable=False)
-    exchange_timestamp: datetime = Field(nullable=False)
