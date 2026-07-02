8ca53b08c4c30489eed9e2e0500f1c358b322c1d
u24603199 <u24603199@tuks.co.za>
2026-07-02 16:29:06 +0200

    Ran linting and pyright

--- backend/app/models/MarketCondition.py
+++ backend/app/models/MarketCondition.py
@@ -2,7 +2,7 @@ from sqlmodel import Field, SQLModel
 from datetime import datetime
 from enum import Enum
 
-class Condition(Enum)
+class Condition(Enum):
     BULL = "BULL"
     BEAR = "BEAR"
     RANGING = "RANGING"
