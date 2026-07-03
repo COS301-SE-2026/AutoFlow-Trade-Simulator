1f7ab1e2646ffcf03ea6ca0642dfd955e9ee2197
u24603199 <u24603199@tuks.co.za>
2026-07-03 17:15:59 +0200

    Added the first testing method

--- backend/tests/test_hist_price.py
+++ backend/tests/test_hist_price.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from decimal import Decimal
 import pytest
+import os
 from dotenv import load_dotenv
 from sqlmodel import Session, SQLModel, create_engine, select
-from hist_price import ../app/models/HistPrice
+from app.models import HistPrice
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -11,4 +12,21 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("Could not find database URL")
 
-@pytest.fixture(name="db_session")
+#Try to connect to the db if you cant connect kill the connection and rollback
+#Upon sucessfull test rollback and kill the connection
+@pytest.fixture(name="hist_price_session")
+def hist_price_session_fixture():
+    engine = create_engine(DATABASE_URL)
+    connection = engine.connect()
+    transaction = connection.begin()
+
+    try:
+        with Session(bind=connection) as session:
+            yield session
+    except Exception as e:
+        transaction.rollback()
+        connection.close()
+        raise e
+
+    transaction.rollback()
+    connection.close()
