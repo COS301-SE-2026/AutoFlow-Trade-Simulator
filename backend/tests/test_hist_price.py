5602b2fdee7b4469d0e7ab7f0e90771e6f2f6860
u24603199 <u24603199@tuks.co.za>
2026-07-03 18:05:34 +0200

    Added storage and reterival testing

--- backend/tests/test_hist_price.py
+++ backend/tests/test_hist_price.py
@@ -4,7 +4,7 @@ import pytest
 import os
 from dotenv import load_dotenv
 from sqlmodel import Session, SQLModel, create_engine, select
-from app.models import HistPrice
+from app.models.HistPrice import HistPrice
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -30,3 +30,42 @@ def hist_price_session_fixture():
 
     transaction.rollback()
     connection.close()
+
+def test_hist_price_storage_and_retrieval(hist_price_session: Session):
+
+    testing_date = datetime(2025, 7, 3, 17, 0, 0)
+
+    new_price_test = HistPrice(
+        asset_id=1,
+        volume=500,
+        open_price=Decimal("120.50"),
+        high_price=Decimal("130.75"),
+        low_price=Decimal("119.80"),
+        symbol="AAPL",
+        offical_close=Decimal("100.20"),
+        date=testing_date
+    )
+
+    hist_price_session.add(new_price_test)
+    hist_price_session.flush()
+
+    #I need to see if the test is being written to the db so pause the execution
+    #note for self press c then enter to resume the test
+    breakpoint()
+
+    #Wipe the cache
+    hist_price_session.expire_all()
+
+    statement = select(HistPrice).where(
+        HistPrice.asset_id == 1,
+        HistPrice.date == testing_date
+    )
+    retrieved_data = hist_price_session.exec(statement).first()
+
+    assert retrieved_data is not None, "Failed to get data."
+    assert retrieved_data.symbol == "AAPL"
+    assert retrieved_data.volume == 500
+    assert retrieved_data.open_price == Decimal("120.50")
+    assert retrieved_data.high_price == Decimal("130.75")
+    assert retrieved_data.offical_close == Decimal("100.20")
+    assert retrieved_data.date == testing_date
