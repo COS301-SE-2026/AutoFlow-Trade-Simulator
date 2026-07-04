e5b5b09aa3a835356ee06d7db3de5158e0b699ee
u24603199 <u24603199@tuks.co.za>
2026-07-04 16:24:05 +0200

    Got hyper table to sucessfully generate!

--- backend/app/models/MockTickerTest.py
+++ backend/app/models/MockTickerTest.py
@@ -5,7 +5,7 @@ from sqlmodel import Field, SQLModel
 class MockTickerTest(SQLModel, table=True):
     
     id:int=Field(default=None, primary_key=True)
-    timestamp:datetime=Field(nullable=False)
+    timestamp:datetime=Field(primary_key=True)
     symbol:str=Field(index=True)
     price:Decimal=Field(nullable=False, max_digits=18, decimal_places=4)
     volume:int=Field(nullable=False)
