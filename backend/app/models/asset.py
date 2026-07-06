e509fbb81f74727b0f456056593f2948955445fa
u24603199 <u24603199@tuks.co.za>
2026-07-06 15:57:10 +0200

    Applied fiexes to Asset table

--- backend/app/models/asset.py
+++ backend/app/models/asset.py
@@ -1,8 +1,9 @@
-
 from typing import Optional
 from sqlmodel import Field, SQLModel
+
 class Asset(SQLModel,table=True):
-    id:Optional[int] = Field(default=None,primary_key=True)
-    ticker:str = Field(default=None)
-    name:str = Field(default=None)
-    asset_type:str = Field(default=None)
+    asset_id:Optional[int] = Field(default=None,primary_key=True)
+    symbol:str=Field(unique=True, max_length=20)
+    asset_class:str=Field(nullable=False, max_length=50)
+    exchange:str=Field(nullable=False, max_length=20)
+    currency:str=Field(nullable=False, min_length=3 ,max_length=3)
