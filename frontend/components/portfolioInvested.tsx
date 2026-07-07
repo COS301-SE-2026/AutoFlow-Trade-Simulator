d17254cc3cde5d109c6545f0030d15ad2090b9d7
u24614582 <u24614582@tuks.co.za>
2026-07-08 01:49:06 +0200

    added basic implementation for viewing value of holdings also showing how many holdings are considered

--- frontend/components/portfolioInvested.tsx
+++ frontend/components/portfolioInvested.tsx
@@ -1,12 +1,29 @@
 'use client';
 import { useAccount } from '@/lib/hooks/accountContext';
+import { useHoldings } from '@/hooks/useHoldings';
+import { useMemo } from 'react';
 
 export function PortfolioInvested({ accountId }: { accountId: number | null }) {
     const { activeAccount } = useAccount();
+    const { holdings, loading, error } = useHoldings(accountId);
+
+    const sumHoldings = useMemo(() => {
+        let temp = 0;
+        for (let index = 0; index < holdings.length; index++) {
+            const holding = holdings[index];
+            temp += holding.current_price * holding.net_quantity;
+        }
+        return temp;
+    }, [holdings]);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>{error}</p>;
 
     return (
         <div>
-            {activeAccount?.balance} {activeAccount?.currency_code}
+            <div>Invested</div>
+            <div>{sumHoldings} {activeAccount?.currency_code}</div>
+            <div>Across {holdings.length} positions</div>
         </div>
     );
 }
