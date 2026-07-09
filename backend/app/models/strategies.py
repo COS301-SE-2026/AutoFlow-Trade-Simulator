5e432d678ec2c65074e4ef8c94bffaec4c003f2b
u24603199 <u24603199@tuks.co.za>
2026-07-09 17:59:27 +0200

    Modfied startegies table with suggestions

--- backend/app/models/strategies.py
+++ backend/app/models/strategies.py
@@ -5,10 +5,4 @@ from typing import Optional
 
 class Strategies(SQLModel, table=True):
     strat_id:Optional[int]=Field(default=None, primary_key=True)
-    user_id:int=Field(foreign_key="user.id")
     name:str=Field(nullable=False, max_length=50)
-    is_active:bool=Field(default=False)
-    description:Optional[str]=Field(nullable=False, max_length=10000)
-    parameters: Optional[dict]= Field(default=None, sa_type=JSONB)
-    created_at:datetime=Field(default_factory=datetime.now)
-    updated_at:Optional[datetime]= Field(default=None)
