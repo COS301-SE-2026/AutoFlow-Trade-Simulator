302440e744bd7d5d56aeaed8fa09af60960d1156
Finnley Wyllie <u24754120@tuks.co.za>
2026-07-13 15:29:17 +0200

    added simulation request DTOs

--- backend/app/epics/simulation/SimulationDTOs.py
+++ backend/app/epics/simulation/SimulationDTOs.py
@@ -1,8 +1,48 @@
-from sqlmodel import  SQLModel
+import decimal
+from typing import Any, Dict, List, Optional
+from sqlmodel import SQLModel
 from ...models.strategies import Strategies
+from decimal import Decimal
+from datetime import date, datetime
 class EpicStatusDTO(SQLModel):
     epic: str
     status: str
 
 class StrategiesResponse(SQLModel):
     strategies: list[Strategies]
+
+class SimulationAction(SQLModel):
+    type:str #buy or sell
+    symbol:str
+    qty: Decimal
+    price: Optional[Decimal] =None
+    timestamp: datetime
+    meta: Optional[Dict[str, Any]] = None
+
+class SimulationCreateRequest(SQLModel):
+    symbols:List[str]
+    allocations:Optional[Dict[str,Decimal]]
+    start_date:date
+    end_date:date
+    initial_balance:Decimal = Decimal('10000')
+    strategey_id:Optional[int]= None
+    persist:bool = True
+
+class SimulationAppendRequest(SQLModel):
+    simulation_id:int
+    actions: List[SimulationAction]
+
+class PerSymbolResult(SQLModel):
+    final_value:Decimal
+    returns_pct:Decimal
+
+class SimulationSummary(SQLModel):
+    final_balance:Decimal
+    returns_pct:Decimal
+    max_drawdown:Decimal
+    trades_count:int
+    per_symbol_results:Dict[str,PerSymbolResult]
+    
+
+
+
