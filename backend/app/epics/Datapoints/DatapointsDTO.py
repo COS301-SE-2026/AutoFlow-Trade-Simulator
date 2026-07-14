4a15b0e57c2ab010ea1d25e28bb82522158fa720
u24603199 <u24603199@tuks.co.za>
2026-07-14 17:38:24 +0200

    Finsihed of the DatapointDTO

--- backend/app/epics/Datapoints/DatapointsDTO.py
+++ backend/app/epics/Datapoints/DatapointsDTO.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from typing import Optional, List
 from pydantic import BaseModel, Field
 
+#validate the json body we are getting feels ood not coding a validator for this stuff...
 class QueryParameters(BaseModel):
     timeframe: str = Field(
         default="1month",
@@ -13,3 +14,16 @@ class QueryParameters(BaseModel):
         le=500,
         description="Number of data points desired for graph"
     )
+
+#DTO to return stuffs
+class DataPoint(BaseModel):
+    time: datetime
+    open: Optional[float] = None
+    high: Optional[float] = None
+    low: Optional[float] = None
+    close: Optional[float] = None
+    volume: float = 0.0
+
+#Allows for the db fields to be changed into smth python will understand
+    class Config:
+        from_attributes = True
