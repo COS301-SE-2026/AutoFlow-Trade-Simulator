9b0d47d6d1b8136c2f0b56c6d2ab566f4824c23c
u24603199 <u24603199@tuks.co.za>
2026-07-14 18:02:26 +0200

    Finished off DatapointsService

--- backend/app/epics/Datapoints/DatapointsService.py
+++ backend/app/epics/Datapoints/DatapointsService.py
@@ -16,12 +16,14 @@ def sampled_ohlcv( session: Session, asset_id: int, params: QueryParameters) -> 
     #A little code to stop a minor error such as not sending the correct interval
     duration = timeframe_mapping.get(params.timeframe, timeframe_mapping["1 month"])
 
+    #Calculations to help get the desired data points
     total_seconds = duration.total_seconds()
     bucket_seconds = int(total_seconds / params.data_points)
     bucket_interval = f"{bucket_seconds} seconds"
 
     start_time = datetime.utcnow() - duration
 
+    #Query to get all desired data alias the names so their a bit more descriptive
     query = text(""
         SELECT
             time_bucket(:bucket_interval, "timestamp") AS bucket_time,
@@ -36,3 +38,25 @@ def sampled_ohlcv( session: Session, asset_id: int, params: QueryParameters) -> 
         GROUP BY bucket_time
         ORDER BY bucket_time ASC;
     "")
+
+    result = session.execute(
+        query,
+        {
+            "bucket_interval": bucket_interval,
+            "asset_id": asset_id,
+            "start_time": start_time
+        }
+    )
+
+    #loop through the raw rows returned by the db making a List of Datapoint then we return the data
+    return [
+        DataPoint(
+            time=row.bucket_time,
+            open=round(float(row.open_price), 4) if row.open_price else None,
+            high=round(float(row.high_price), 4) if row.high_price else None,
+            low=round(float(row.low_price), 4) if row.low_price else None,
+            close=round(float(row.close_price), 4) if row.close_price else None,
+            volume=round(float(row.total_volume), 4) if row.total_volume else None,
+        )
+        for row in result
+    ]
