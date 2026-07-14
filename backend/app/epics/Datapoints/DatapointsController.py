1bd6b0347a5a6063ea13c3b01f4d0f0c2c4125b9
u24603199 <u24603199@tuks.co.za>
2026-07-14 20:40:50 +0200

    Sonar qube issues fixed hopefully

--- backend/app/epics/Datapoints/DatapointsController.py
+++ backend/app/epics/Datapoints/DatapointsController.py
@@ -5,10 +5,13 @@ from .DatapointsService import sampled_ohlcv
 from .DatapointsDTO import QueryParameters
 from ...database import get_session
 
+SessionDep = Annoted[Session, Depends(get_session)]
+QueryDep = Annoted[QueryParameters, Depends()]
+
 router = APIRouter(prefix="/assets", tags=["Charts"])
 
 #Nvm I can pass session should really read code a little slower
 
 @router.get("/{asset_id}/chart")
-def get_asset_chart(asset_id: int, params: QueryParameters = Depends(), session: Session = Depends(get_session)):
+def get_asset_chart(asset_id: int, params: QueryDep, session: SessionDep):
     return sampled_ohlcv(session=session, asset_id=asset_id, params=params)
