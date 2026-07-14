55d645526249dbf580b4d5b586d94da82b3fd820
u24603199 <u24603199@tuks.co.za>
2026-07-14 18:21:55 +0200

    Added note for self

--- backend/app/epics/Datapoints/DatapointsController.py
+++ backend/app/epics/Datapoints/DatapointsController.py
@@ -7,6 +7,9 @@ from DatapointsDTO import QueryParameters, DataPoint
 
 router = APIRouter(prefix"/assets", tags=["Charts"])
 
+#I cant pass session around to much headache saw a cool way to test using it by means of mocking the db
+#After some consideration not worth it...
+
 @router.get("/{asset_id}/chart")
     def get_asset_chart( asset_id: int, params: QueryParameters = Depends()):
         return sampled_ohlcv(asset_id=asset_id, params=params)
