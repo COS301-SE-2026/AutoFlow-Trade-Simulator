e1f407fc40a5e4fc787d0dff5c30b175cc7ee26d
u24603199 <u24603199@tuks.co.za>
2026-07-14 17:54:58 +0200

    Added SQL for the data point calculation

--- backend/app/epics/Datapoints/DatapointsService.py
+++ backend/app/epics/Datapoints/DatapointsService.py
@@ -15,3 +15,24 @@ def sampled_ohlcv( session: Session, asset_id: int, params: QueryParameters) -> 
 
     #A little code to stop a minor error such as not sending the correct interval
     duration = timeframe_mapping.get(params.timeframe, timeframe_mapping["1 month"])
+
+    total_seconds = duration.total_seconds()
+    bucket_seconds = int(total_seconds / params.data_points)
+    bucket_interval = f"{bucket_seconds} seconds"
+
+    start_time = datetime.utcnow() - duration
+
+    query = text(""
+        SELECT
+            time_bucket(:bucket_interval, "timestamp") AS bucket_time,
+            first(open, "timestamp") AS open_price,
+            max(high) AS high_price,
+            min(low) AS low_price,
+            last(close, "timestamp") AS close_price,
+            sum(volume) AS total_volume
+        FROM dailyohlcv
+        Where asset_id = :asset_id
+            AND "timestamp" >= :start_time
+        GROUP BY bucket_time
+        ORDER BY bucket_time ASC;
+    "")
