d52807e714fddf8bfba413b664816bc8b743134d
Finnley Wyllie <u24754120@tuks.co.za>
2026-07-15 12:12:24 +0200

    added helper function and started backtest engine implementation

--- backend/app/epics/simulation/SimulationService.py
+++ backend/app/epics/simulation/SimulationService.py
@@ -1,12 +1,19 @@
 
+from datetime import date
 from decimal import Decimal
-from typing import List
+from typing import List, Optional
+from sqlalchemy import Select
+from sqlalchemy.engine import result
 from sqlmodel import Session, select
 
 from ...models.strategies import Strategies
-from .SimulationDTOs import StrategiesResponse, EpicStatusDTO
+from ...models.daily_OHLCV import DailyOHLCV
+from ...models.asset import Asset
+from .SimulationDTOs import SimulationCreateRequest, SimulationFinishResponse, StrategiesResponse, EpicStatusDTO
 
-
+MAX_SYMBOLS = 20
+MAX_YEARS = 5
+MAX_ROWS = 100_000
 
 def compute_max_drawdown(nav_series:List[Decimal])->Decimal:
     if not nav_series:
@@ -36,4 +43,35 @@ class SimulationService:
     def get_strategies(self)->StrategiesResponse:
         strategies= self.session.exec(select(Strategies)).all()
         return StrategiesResponse(strategies=list(strategies))
+    
+    def validate_limits(self,symbol:List[str],start:date,end:date):
+        if len(symbol)>MAX_SYMBOLS:
+            raise ValueError(f"Max symbols is {MAX_SYMBOLS}")
+        if (end-start).days>(MAX_YEARS*365):
+            raise ValueError("Date range too large")
+
+    def load_bars(self,symbol:str,start:date,end:date)-> List[DailyOHLCV]:
+        asset_id= self.session.exec(select(Asset.asset_id).where(Asset.symbol== symbol)).first()
+        if asset_id is None:
+            raise ValueError("Symbols doesnt exist")
+        result:list= list(self.session.exec(select(DailyOHLCV).where(DailyOHLCV.asset_id==asset_id)).all())
+        return result
+
+    def run_backtest(self,req:SimulationCreateRequest,user_id:Optional[int])->SimulationFinishResponse:
+        self.validate_limits(req.symbols,req.start_date,req.end_date)
+
+        #load bars and check rows
+        bars_by_symbol={}
+        total_rows=0
+        for s in req.symbols:
+            bars =self.load_bars(s,req.start_date,req.end_date)
+            bars_by_symbol[s]= bars
+            total_rows+=len(bars)
+            if total_rows>MAX_ROWS:
+                raise ValueError("Requested data too large")
+
+
+
+
+
 
