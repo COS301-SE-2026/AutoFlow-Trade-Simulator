7d43244aafcd9ab163544e61031cfb0028922c70
u24603199 <u24603199@tuks.co.za>
2026-07-18 16:58:37 +0200

    replaced requested fields

--- backend/app/models/options.py
+++ backend/app/models/options.py
@@ -12,4 +12,4 @@ class options(SQLModel, table=True):
     option_type: option_type = Field(nullable=False)
     strike_price: Decimal = Field(max_digits=18, decimal_places=4, nullable=False)
     expr_date: datetime = Field(nullable=False)
-    exercise_style: str = Field(default="None", max_length=20, nullable=False)
+    imp_vol: Decimal = Field(default="None", max_digits=8, decimal_places=4, nullable=False)
