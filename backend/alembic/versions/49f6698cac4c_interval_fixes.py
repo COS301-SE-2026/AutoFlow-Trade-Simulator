e7b4d745996d599f8aa9fa07216a1c6c43acabe7
u24603199 <u24603199@tuks.co.za>
2026-07-21 23:10:15 +0200

    Hopefully added the last fixes

--- backend/alembic/versions/49f6698cac4c_interval_fixes.py
+++ backend/alembic/versions/49f6698cac4c_interval_fixes.py
@@ -64,7 +64,7 @@ def upgrade() -> None:
         CREATE MATERIALIZED VIEW IF NOT EXISTS ohlcv_1h
         WITH(timescaledb.continuous, timescaledb.materialized_only = false) AS
         SELECT
-            time_bucket('1 hour', "timestamp") AS bucket_time,
+            time_bucket(INTERVAL '1 hour', "timestamp") AS bucket_time,
             asset_id,
             first(open, "timestamp") AS open,
             max(high) AS high,
@@ -142,15 +142,15 @@ def upgrade() -> None:
         CREATE MATERIALIZED VIEW IF NOT EXISTS ohlcv_1m
         WITH (timescaledb.continuous, timescaledb.materialized_only = false) AS 
         SELECT
-            time_bucket(INTERVAL '1 month', bucket_time) AS bucket_time,
+            time_bucket(INTERVAL '1 month', "timestamp") AS bucket_time,
             asset_id,
-            first(open, bucket_time) AS open,
+            first(open, "timestamp") AS open,
             max(high) AS high,
             min(low) AS low,
-            last(close, bucket_time) AS close,
+            last(close, "timestamp") AS close,
             sum(volume) AS volume
-        From ohlcv_1d
-        GROUP BY time_bucket(INTERVAL '1 month', bucket_time), asset_id
+        From dailyohlcv
+        GROUP BY time_bucket(INTERVAL '1 month', "timestamp"), asset_id
         WITH NO DATA;
     """)
 
@@ -170,15 +170,15 @@ def upgrade() -> None:
         CREATE MATERIALIZED VIEW IF NOT EXISTS ohlcv_6m
         WITH (timescaledb.continuous, timescaledb.materialized_only = false) AS 
         SELECT
-            time_bucket(INTERVAL '6 months', bucket_time) AS bucket_time,
+            time_bucket(INTERVAL '6 months', "timestamp") AS bucket_time,
             asset_id,
-            first(open, bucket_time) AS open,
+            first(open, "timestamp") AS open,
             max(high) AS high,
             min(low) AS low,
-            last(close, bucket_time) AS close,
+            last(close, "timestamp") AS close,
             sum(volume) AS volume
-        From ohlcv_1d
-        GROUP BY time_bucket(INTERVAL '6 months', bucket_time), asset_id
+        From dailyohlcv
+        GROUP BY time_bucket(INTERVAL '6 months', "timestamp"), asset_id
         WITH NO DATA;
     """)
 
@@ -197,15 +197,15 @@ def upgrade() -> None:
         CREATE MATERIALIZED VIEW IF NOT EXISTS ohlcv_1y
         WITH (timescaledb.continuous, timescaledb.materialized_only = false) AS 
         SELECT
-            time_bucket(INTERVAL '1 year', bucket_time) AS bucket_time,
+            time_bucket(INTERVAL '1 year', "timestamp") AS bucket_time,
             asset_id,
-            first(open, bucket_time) AS open,
+            first(open, "timestamp") AS open,
             max(high) AS high,
             min(low) AS low,
-            last(close, bucket_time) AS close,
+            last(close, "timestamp") AS close,
             sum(volume) AS volume
-        From ohlcv_1d
-        GROUP BY time_bucket(INTERVAL '1 year', bucket_time), asset_id
+        From dailyohlcv
+        GROUP BY time_bucket(INTERVAL '1 year', "timestamp"), asset_id
         WITH NO DATA;
     """)
 
