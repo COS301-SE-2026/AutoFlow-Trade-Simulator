abc72719b1839d7d25832a39ecaf09675720a787
GC Nel <u24574547@tuks.co.za>
2026-07-21 22:04:34 +0200

    Corrected database errors and naming convention violations

--- backend/app/models/options.py
+++ backend/app/models/options.py
@@ -3,15 +3,15 @@ from datetime import datetime, date
 from enum import Enum
 from decimal import Decimal
 
-class option_type(Enum):
+class OptionType(Enum):
     CALL = "CALL"
     PUT = "PUT"
 
-class options(SQLModel, table=True):
+class Options(SQLModel, table=True):
     contract_symbol: str = Field(max_length=32, primary_key=True)
     timestamp: datetime = Field(primary_key=True)
     asset_id: int = Field(foreign_key="asset.asset_id", ondelete="CASCADE", index=True, nullable=False)
-    option_type: option_type = Field(nullable=False)
+    option_type: OptionType = Field(nullable=False)
     strike_price: Decimal = Field(max_digits=18, decimal_places=4, nullable=False)
     expr_date: date = Field(nullable=False)
     bid: Decimal = Field(max_digits=18, decimal_places=4, nullable=True)
