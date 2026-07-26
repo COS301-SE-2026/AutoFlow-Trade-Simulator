bf161749f94a587eec11c00febeee38056536e51
u24614582 <u24614582@tuks.co.za>
2026-07-26 19:11:59 +0200

    reduced repitition in help menu page by using an array mapping instead. also made the tutorial section accept image text pairs so its easier to follow

--- frontend/app/help/page.tsx
+++ frontend/app/help/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Navbar } from '@/components/navbar';
+import Image from 'next/image';
 
 export default function LearningPage() {
   return (
@@ -15,44 +16,72 @@ export default function LearningPage() {
 
         <div className='text-4xl flex justify-center mb-3'>tutorial</div>
         <div className='space-2 bg-[var(--background)] border border-[var(--border)] rounded-xl p-6 hover:border-[var(--purple)] transition-colors'>
-          placehodler
+          {[
+            // array containing tutorial objects. objects contain question string and answer array. answer array holds answer objects. answer objects contain answer and image reference strings
+            {
+              q: 'how to create an account', a: [
+                { text: '', image: '/logo.svg' },
+                { text: '', image: '/logo.svg' },
+                { text: '', image: '/logo.svg' },
+              ]
+            },
+            {
+              q: 'how to buy and sell stocks', a: [
+                { text: '', image: '/logo.svg' },
+                { text: '', image: '/logo.svg' },
+                { text: '', image: '/logo.svg' },
+              ]
+            },
+            {
+              q: 'how to learn about strategies', a: [
+                { text: '', image: '/logo.svg' },
+                { text: '', image: '/logo.svg' },
+                { text: '', image: '/logo.svg' },
+              ]
+            },
+            {
+              q: 'how to view account details', a: [
+                { text: '', image: '/logo.svg' },
+                { text: '', image: '/logo.svg' },
+                { text: '', image: '/logo.svg' },
+              ]
+            },
+          ].map((f, index) => (
+            // mapping array to divs
+            <div key={`empty-${index}`}>
+              <p className='text-1.5x1 mb-3'>{index + 1}. {f.q}</p>
+              {f.a.map((a, index) => (
+                // mapping answer array to text image pairs
+                <div key={`empty-${index}`}>
+                  <p className='mb-4' style={{ color: 'var(--muted' }}>{a.text}</p>
+                  {a.image !== '' && (
+                    <Image
+                      src={a.image}
+                      alt='Autoflow'
+                      width={24}
+                      height={24}
+                    />
+                  )}
+                </div>
+              ))}
+            </div>
+          ))}
         </div>
         <hr className='border-[#9ca3af] my-6' />
 
         <div className='text-4xl flex justify-center mb-3'>FAQ</div>
         <div className='space-2 bg-[var(--background)] border border-[var(--border)] rounded-xl p-6 hover:border-[var(--purple)] transition-colors'>
-          <div>
-            <p className='text-1.5x1 mb-3'>1. what is AutoFlow Trade Simulator</p>
-            <p style={{ color: 'var(--muted' }}>AutoFlow Trade Simulator is a learning program that seeks to teach trading in a stress free and intuitive way.</p>
-          </div>
-          <div>
-            <p className='text-1.5x1 mb-3'>2. is the money real?</p>
-            <p style={{ color: 'var(--muted' }}>no, the money and value shown in AutoFlow Trade Simulator exists purely for education. however, real market data is used for learning and allows users to practice in real world situations</p>
-          </div>
-          <div>
-            <p className='text-1.5x1 mb-3'>3. how do i start learning?</p>
-            <p style={{ color: 'var(--muted' }}>the learning tab at the <a href='/learning' className='text-blue-400'> learning tab</a> provides a list of typical trading strategies and an environment to practice their use in real historical situations.</p>
-          </div>
-          <div>
-            <p className='text-1.5x1 mb-3'>4. what are greeks?</p>
-            <p style={{ color: 'var(--muted' }}>greeks are risk metrics used in options trading. you can learn more by going to the <a href='/learning' className='text-blue-400'> learning tab</a> and selecting the greeks tab or by experimenting on the simulation tab found on the same page</p>
-          </div>
-          <div>
-            <p className='text-1.5x1 mb-3'>5. how do i buy and sell stocks?</p>
-            <p style={{ color: 'var(--muted' }}>create an account and go to the <a href='/#' className='text-blue-400'> market tab</a> to begin trading</p>
-          </div>
-          <div>
-            <p className='text-1.5x1 mb-3'>6. how do i learn and practice strategies?</p>
-            <p style={{ color: 'var(--muted' }}>go to the <a href='/learning' className='text-blue-400'> learning tab</a> and select the tab for strategies using the tab selector</p>
-          </div>
-          <div>
-            <p className='text-1.5x1 mb-3'>7. where can i see all my money?</p>
-            <p style={{ color: 'var(--muted' }}>you can track the value of your accounts on the <a href="/portfolio" className='text-blue-400'> portfolio page</a></p>
-          </div>
-          <div>
-            <p className='text-1.5x1 mb-3'>8. how do i make an account?</p>
-            <p style={{ color: 'var(--muted' }}>you can make a practice account by interacting with the dropdown on the top right of the screen and choosing a currency and starting amount.</p>
-          </div>
+          {[
+            { q: 'is my money real?', a: 'no, money on autoflow trading simulator does not hold any real value and exists the help you practice trading.' },
+            { q: 'how do i make more money?', a: 'you can make more money by investing in certain holdings which increase in value or create a new account with a starting amount of your choosing.' },
+            { q: 'what is stop loss?', a: 'stop loss is an order to sell your stock when it reaches a specific price so you are protected from losing too much of your investment.' },
+            { q: 'is the data real?', a: 'yes, data on autoflow trading simulator uses historical and real time market data to make learning more engaging and effective.' },
+          ].map((f, index) => (
+            <div key={`empty-${index}`}>
+              <p className='text-1.5x1 mb-3'>{index + 1}. {f.q}</p>
+              <p className='mb-4' style={{ color: 'var(--muted' }}>{f.a}</p>
+            </div>
+          ))}
         </div>
         <hr className='border-[#9ca3af] my-6' />
       </div >
