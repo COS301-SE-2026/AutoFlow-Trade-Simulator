b29f829b540d74f844fa1b834cf1e863a235c7de
u24614582 <u24614582@tuks.co.za>
2026-07-27 01:16:06 +0200

    made getTutorial to make tutorial instead of restating object structure repeatedly

--- frontend/app/help/page.tsx
+++ frontend/app/help/page.tsx
@@ -4,39 +4,44 @@ import { Navbar } from '@/components/navbar';
 import Image from 'next/image';
 
 export default function LearningPage() {
-  const tutorial = [
+  const getTutorial = (id: string, q: string, a: { id: string, text: string, image: string }[]) => ({
+    id: id,
+    q: q,
+    a: a.map((step) => ({
+      id: step.id,
+      text: step.text,
+      image: step.image,
+    }))
+  })
+
+  const tutorials = [
     // array containing tutorial objects. objects contain question string and answer array. answer array holds answer objects. answer objects contain answer and image reference strings
-    {
-      id: `1`,
-      q: 'how to create an account', a: [
+    getTutorial('1', 'how to create an account',
+      [
         { id: '1', text: 'after logging in, click on the "add accounts" button visible on the top right of the screen in the navigation bar', image: '/help/create-account-0.png' },
         { id: '2', text: 'choose a currency and set a starting amount', image: '/help/create-account-1.png' },
         { id: '3', text: 'then click confirm to create an account', image: '/help/create-account-2.png' },
       ]
-    },
-    {
-      id: '2',
-      q: 'how to buy and sell stocks', a: [
+    ), getTutorial('2', 'how to buy and sell stocks',
+      [
         { id: '1', text: 'navigate to the market page using the navigation bar', image: '/help/buy-sell-0.png' },
         { id: '2', text: 'choose either select or buy and enter a quantity by either using the input box or MAX to sell or buy the maximum amount of stock', image: '/help/buy-sell-1.png' },
         { id: '3', text: 'click the buy or sell button at the bottom of the page', image: '/help/buy-sell-2.png' },
       ]
-    },
-    {
-      id: '3',
-      q: 'how to learn about strategies', a: [
+    ), getTutorial('3', 'how to learn about strategies',
+      [
         { id: '1', text: 'go to the learning tab and select strategies', image: '/help/strategy-learning-0.png' },
         { id: '2', text: 'choose a strategy you wish to learn or filter them using the selectors', image: '/help/strategylist.png' },
         { id: '3', text: 'read about the strategies and practice them in the historical simulation page', image: '/help/strategydetail.png' },
       ]
-    },
-    {
-      id: '4',
-      q: 'how to view account details', a: [
+    ),
+    getTutorial('4', 'how to view account details',
+      [
         { id: '1', text: 'navigate to the portfolio page using the navigation bar', image: '/help/portfolio-0.png' },
         { id: '2', text: 'the cash balance, amount invested and total value are are visible', image: '/help/portfolio.png' },
       ]
-    },
+    ),
+
   ];
 
   const faq = [
@@ -58,11 +63,11 @@ export default function LearningPage() {
 
         <div className='text-4xl flex justify-center mb-3'>tutorial</div>
         <div className='space-2 bg-[var(--background)] border border-[var(--border)] rounded-xl p-6 hover:border-[var(--purple)] transition-colors'>
-          {tutorial.map((t) => (
-            <div key={`tutorial-${t.id}`}>
+          {tutorials.map((t) => (
+            <div key={`tutorials-${t.id}`}>
               <p className='text-1.5x1 mb-3'>{t.id}. {t.q}</p>
               {t.a.map((a) => (
-                <div key={`tutorial-${t.id}-step-${a.id}`}>
+                <div key={`tutorials-${t.id}-step-${a.id}`}>
                   <p className='mb-4' style={{ color: 'var(--muted' }}>{a.text}</p>
                   <Image
                     src={a.image}
