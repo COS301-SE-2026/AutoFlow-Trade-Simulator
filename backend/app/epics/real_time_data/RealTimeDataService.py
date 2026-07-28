08565e4d527e4ca7cf91f7febf70a3c8b04bc667
Finnley Wyllie <u24754120@tuks.co.za>
2026-07-29 00:55:35 +0200

    fixed a logical error that would load unwanted ticker data

--- backend/app/epics/real_time_data/RealTimeDataService.py
+++ backend/app/epics/real_time_data/RealTimeDataService.py
@@ -1,4 +1,6 @@
-from sqlmodel import   Session, col, select
+from datetime import datetime, timedelta
+from fastapi import HTTPException, status
+from sqlmodel import   Session,select
 
 from .RealTimeDataDTOs import DataResponseDTO, EpicStatusDTO
 from ...models.real_time_ticks import RealTimeTicks
@@ -16,10 +18,13 @@ class RealTimeDataService:
         )
 
     def get_real_time_data(self, symbol: str) -> DataResponseDTO:
-        asset_id:int|None= self.session.exec(select(Asset.asset_id).where(Asset.symbol==symbol)).one()
+        asset_id:int|None= self.session.exec(select(Asset.asset_id).where(Asset.symbol==symbol)).first()
         if asset_id is None:
-            raise ValueError(f"Asset not found for symbol: {symbol}")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Asset not found for symbol: {symbol}")
 
-        points: list[RealTimeTicks] =list(self.session.exec(select(RealTimeTicks).where(RealTimeTicks.asset_id==asset_id)).all())
+        today:datetime= datetime.utcnow()
+        yesterday:datetime = today+timedelta(days=-1)
+
+        points: list[RealTimeTicks] =list(self.session.exec(select(RealTimeTicks).where(RealTimeTicks.asset_id==asset_id).where(RealTimeTicks.timestamp>yesterday)).all())
         return DataResponseDTO(points=points)
 
