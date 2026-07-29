97fd5f7e14719525bf4fd726cfcfa94e436b6006
Finnley Wyllie <u24754120@tuks.co.za>
2026-07-29 02:19:38 +0200

    fixed exposed asset_d

--- backend/app/epics/real_time_data/RealTimeDataDTOs.py
+++ backend/app/epics/real_time_data/RealTimeDataDTOs.py
@@ -1,10 +1,15 @@
-
+from decimal import Decimal
 from sqlmodel import SQLModel
 from ...models.real_time_ticks import RealTimeTicks
+from datetime import datetime
 
+class DataPoint(SQLModel):
+    timestamp:datetime
+    price:Decimal
+    volume:Decimal
 
 class DataResponseDTO(SQLModel):
-    points:list[RealTimeTicks]
+    points:list[DataPoint]
 
 class EpicStatusDTO(SQLModel):
     epic: str
