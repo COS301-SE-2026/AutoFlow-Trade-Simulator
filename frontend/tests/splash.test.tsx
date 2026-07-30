53d45d49fa637e0cd991a219081fe83e15a4db0c
u24614582 <u24614582@tuks.co.za>
2026-07-31 01:21:29 +0200

    moved splash page test to be on root (new splash page)

--- frontend/tests/splash.test.tsx
+++ frontend/tests/splash.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import SplashPage from '@/app/splash/page';
+import SplashPage from '@/app/page';
 
 describe('SplashPage', () => {
 
