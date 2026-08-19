557e8b60e62f68d120fcd8e451c9a4bacb44ec24
Finnley Wyllie <u24754120@tuks.co.za>
2026-08-19 16:05:39 +0200

    replaced a tag with link

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -3,16 +3,17 @@
 import { SignupForm } from "@/components/signup-form"
 import { GalleryVerticalEndIcon } from "lucide-react"
 import Image from "next/image"
+import Link from "next/link"
 export default function SignupPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="/" className="flex items-center gap-2 self-center font-medium">
+        <Link href="/" className="flex items-center gap-2 self-center font-medium">
           <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
             <Image src='/logo.svg' alt='Autoflow' width={24} height={24} className='w-6 w-6' />
           </div>
           Autoflow
-        </a>
+        </Link>
         <SignupForm />
       </div>
     </div>
