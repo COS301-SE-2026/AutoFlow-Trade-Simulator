ce317f9ad85d81103110f6b214378825746ea488
Finnley Wyllie <u24754120@tuks.co.za>
2026-08-19 15:41:47 +0200

    fixed visual issues and updated shadcn globals

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -5,22 +5,17 @@ import { GalleryVerticalEndIcon } from "lucide-react"
 import Image from "next/image"
 
 export default function LoginPage() {
+
   return (
-    <div className="grid min-h-svh">
-      <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
-          <a href="/" className="flex items-center gap-2 font-medium">
-            <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <Image src='/logo.svg' alt='Autoflow' width={24} height={24} className='w-6 w-6' />
-            </div>
-            Autoflow
-          </a>
-        </div>
-        <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
-            <LoginForm />
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="/" className="flex items-center gap-2 self-center font-medium">
+          <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            <Image src='/logo.svg' alt='Autoflow' width={24} height={24} className='w-6 w-6' />
           </div>
-        </div>
+          Autoflow
+        </a>
+        <LoginForm />
       </div>
     </div>
   )
--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -5,21 +5,15 @@ import { GalleryVerticalEndIcon } from "lucide-react"
 import Image from "next/image"
 export default function SignupPage() {
   return (
-    <div className="grid min-h-svh">
-      <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
-          <a href="/" className="flex items-center gap-2 font-medium">
-            <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <Image src='/logo.svg' alt='Autoflow' width={24} height={24} className='w-6 w-6' />
-            </div>
-            Autoflow
-          </a>
-        </div>
-        <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
-            <SignupForm />
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="/" className="flex items-center gap-2 self-center font-medium">
+          <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            <Image src='/logo.svg' alt='Autoflow' width={24} height={24} className='w-6 w-6' />
           </div>
-        </div>
+          Autoflow
+        </a>
+        <SignupForm />
       </div>
     </div>
   )
