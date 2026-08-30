aab86f2723289388ea00d3f69d152d100efa98db
Finnley Wyllie <u24754120@tuks.co.za>
2026-08-30 14:46:57 +0200

    created dtos for news

---
+++ backend/app/epics/news/NewsDTOs.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from typing import Optional
+from sqlmodel import SQLModel
+
+from ...models.news import NewsCategory
+
+
+class NewsItem(SQLModel):
+    id:int
+    timestamp:datetime
+    category:NewsCategory
+    description:str
+    source:Optional[str]
+    author:Optional[str]
+    full_story:str
+
+class NewsRepsonse(SQLModel):
+    news_items: list[NewsItem]
+
+class NewsRequest(SQLModel):
+    start_date:datetime
+    end_date:datetime
+    ticker:str
