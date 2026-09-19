3b5930717b3b9319a727771dbd76f34ce7d95021
u24603199 <u24603199@tuks.co.za>
2026-09-19 14:53:30 +0200

    Added a trigger for adding nodes into the DAG

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,6 +1,7 @@
 from typing import Optional
-
-from sqlmodel import Field, SQLModel
+from sqlalchemy.dialects.postgresql import ARRAY
+from sqlmodel import Field, SQLModel, Column
+import sqlalchemy as sa
 
 
 class User(SQLModel, table=True):
@@ -9,3 +10,9 @@ class User(SQLModel, table=True):
     password_hash: Optional[str] = Field(default=None, nullable=True)
     full_name: str = Field(nullable=False)
     google_sub: Optional[str] = Field(default=None, index=True, nullable=True, sa_column_kwargs={"unique": True})
+    currency: int = Field(default=0, nullable=False)
+    elo: int = Field(default=0, nullable=False)
+    upgrades: List[str] = Field(
+        default_factory=list,
+        sa_column=Column(ARRAY(sa.String), nullable=False, server_default="{}")
+    )
