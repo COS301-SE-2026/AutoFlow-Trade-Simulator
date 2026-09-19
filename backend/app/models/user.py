20ff435a6b773c0ab280567b271ceeeccec867ad
u24603199 <u24603199@tuks.co.za>
2026-09-19 16:47:17 +0200

    Finished adding modified user table and triggers and fixing some one's error of not dropping defined custom types

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -10,8 +10,8 @@ class User(SQLModel, table=True):
     password_hash: Optional[str] = Field(default=None, nullable=True)
     full_name: str = Field(nullable=False)
     google_sub: Optional[str] = Field(default=None, index=True, nullable=True, sa_column_kwargs={"unique": True})
-    currency: int = Field(default=0, nullable=False)
-    elo: int = Field(default=0, nullable=False)
+    currency: int = Field(default=0, nullable=False, sa_column_kwargs={"server_default": "0"})
+    elo: int = Field(default=0, nullable=False, sa_column_kwargs={"server_default": "0"})
     upgrades: list[str] = Field(
         default_factory=list,
         sa_column=Column(ARRAY(sa.String), nullable=False, server_default="{}")
